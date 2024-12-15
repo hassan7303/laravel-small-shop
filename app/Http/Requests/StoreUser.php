ea4d687353580a88f8107ff35918664d4e341562
hsnali <hassanali7303@gmail.com>
2024-12-16 02:25:38 +0330

    added form request Store User

---
+++ app/Http/Requests/StoreUser.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUser extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     * 
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     * 
+     * @return array<string, string>
+     */
+    public function messages():array 
+    {
+        //پیام فارسی برای ذخیره کاربر 
+        return [
+            'name.required' => 'نام الزامی است',
+            'name.max' => 'نام حداکثر 255 کاراکتر می باشد',
+            'name.string' => 'نام باید از نوع رشته باشد',
+            'email.required' => 'ایمیل الزامی است',
+            'email.max' => 'ایمیل حداکثر 255 کاراکتر می باشد',
+            'email.string' => 'ایمیل باید از نوع رشته باشد',    
+        ];
+
+    }
+}
