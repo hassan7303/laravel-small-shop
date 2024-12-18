7e220e6081f9f7806677271ffbc2a391fdeebf56
hsnali <hassanali7303@gmail.com>
2024-12-18 21:03:04 +0330

    fix: change category_id to nullable and added image

--- database/migrations/2024_12_13_062135_create_products_table.php
+++ database/migrations/2024_12_13_062135_create_products_table.php
@@ -12,10 +12,11 @@ return new class extends Migration {
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->char('name');
+            $table->string('name');
             $table->text('description');
             $table->unsignedBigInteger('price');
-            $table->unsignedBigInteger('category_id');
+            $table->string('image')->default('default.jpg');
+            $table->unsignedBigInteger('category_id')->nullable();
             $table->timestamps();
 
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
